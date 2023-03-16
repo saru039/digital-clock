3e096bba9f0ab8d1103544e7ea27103d4845bed2
sarumathi <110803703+saru039@users.noreply.github.com>
2023-03-16 13:28:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+let ampm = document.getElementById('ampm');
 
+function displayTime(){
+  let dateTime = new Date();
+  let hr = dateTime.getHours();
+  let min = padZero(dateTime.getMinutes());
+  let sec = padZero(dateTime.getSeconds());
+  if(hr>12){
+    hr = hr-12;
+    ampm.innerHTML= 'PM';
+    }
+  document.getElementById('hour').innerHTML= padZero(hr);
+  document.getElementById('min').innerHTML= min;
+  document.getElementById('sec').innerHTML= sec;
+
+  }
+  function padZero(num){
+    return num>10?'0'+num:num;
+    }
+setInterval(displayTime, 500);
